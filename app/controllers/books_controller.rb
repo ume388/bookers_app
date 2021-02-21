2b6b1cd462b34a2bfc078ba29c570714ec8ee975
ume388 <turi388ouji@gmail.com>
2021-02-21 07:33:56 +0000

    [fix,add]コントローラの定義

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,6 +4,7 @@ class BooksController < ApplicationController
  
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def show
@@ -17,13 +18,22 @@ class BooksController < ApplicationController
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to books_path
+    redirect_to book_path
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
-  
+
+  def update
+    book = Book.find(params[:id])
+    redirect_to book_path(params[:id])
+  end
+    
   def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
   end
   
   private
