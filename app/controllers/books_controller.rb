7b58f789f2a75cf9cf68cdee92841379e741e6fb
ume388 <turi388ouji@gmail.com>
2021-02-21 07:43:28 +0000

    [fix]コントローラ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -18,7 +18,7 @@ class BooksController < ApplicationController
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to book_path
+    redirect_to book_path(book)
   end
 
   def edit
