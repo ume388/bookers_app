75a41e85fd40c099bebdeffef49a85ade60c8c68
ume388 <turi388ouji@gmail.com>
2021-02-23 01:54:19 +0000

    [add]削除成功メッセージ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -33,7 +33,7 @@ class BooksController < ApplicationController
   def destroy
     book = Book.find(params[:id])
     book.destroy
-    redirect_to books_path
+    redirect_to books_path, notice: 'Book was successfully destroyed.'
   end
   
   private
